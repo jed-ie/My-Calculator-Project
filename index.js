cbca9db58df0a923d459797fccada1f0970e8c72
Jed James <jamesjed211@gmail.com>
2026-07-01 13:09:30 +0100

    number vs operator filtering function

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const buttons = document.querySelector('.buttons');
+const display = document.querySelector('#display-text');
 
+// function to check if value is a number or not
+function isNumber(n){
+    // at first, we need to convert the variable from string to number
+    let new_value = Number(n);
+    // then we need to check if the new value is a number or not
+    if(isNaN(new_value)){
+        return false;
+    }else{
+        return true;
+    }
+}
+// to listen for any click event on the buttons container and log the inner text of the clicked button
 buttons.addEventListener("click",function(e){
-    console.log(e.target.innerText);
+
+    // to detect if what was clicked is a number or not
+    let cap_value = e.target.innerText;
+
+    // we need to detect if its a number, and if its  number, then it should be sent to the display
+    if(isNumber(cap_value)){
+        display.innerHTML = cap_value;
+    }
 });
 
